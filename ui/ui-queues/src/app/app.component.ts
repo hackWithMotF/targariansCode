b29635a6e47a92f64eb62fc56b9807dbae52aeb6
Srikiran Konduri <skonduri@IM-LP-136.local>
2019-08-10 13:38:39 +0530

    ng md:wq!

--- ui/ui-queues/src/app/app.component.ts
+++ ui/ui-queues/src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-
+import { OnInit, Component } from '@angular/core';
+import {DetailComponentComponent} from './detail-component/detail-component.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'ui-queues';
+  ngOnInit() {
+    
+  }
 }
--- ui/ui-queues/src/app/app.module.ts
+++ ui/ui-queues/src/app/app.module.ts
@@ -2,24 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatInputModule, MatPaginatorModule, MatProgressSpinnerModule, 
   MatSortModule, MatTableModule } from "@angular/material"
+  import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DetailComponentComponent } from './detail-component/detail-component.component';
+import {RouterModule, Routes} from '@angular/router';
 
+const appRoutes: Routes =  [
+  {path:'', component: DetailComponentComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
     DetailComponentComponent
   ],
   imports: [
+    // BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
     MatInputModule,
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
