930ecc629d8ea54bff2d1c93605b53a14c83ac2f
Surya Kasinadh <suryakasinadh@gmail.com>
2019-08-10 15:35:32 +0530

    unit test cases

--- service/src/test/java/com/dbs/mq26/Mq26ApplicationTests.java
+++ service/src/test/java/com/dbs/mq26/Mq26ApplicationTests.java
@@ -2,15 +2,41 @@ package com.dbs.mq26;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.dbs.mq26.controller.QueueController;
+import com.dbs.mq26.service.QueueService;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(QueueController.class)
 public class Mq26ApplicationTests {
+
+	@Autowired
+	private MockMvc mvc; 
 
 	@Test
 	public void contextLoads() {
+	}
+	@MockBean
+	private QueueService queueService;
+	@Test
+	public void testGetAllQueues() throws Exception
+	{
+		mvc.perform( MockMvcRequestBuilders
+				.get("/api/queue/getAllQueues")
+				.accept(MediaType.APPLICATION_JSON))
+		.andDo(print())
+		.andExpect(status().isOk())
+		.andExpect(MockMvcResultMatchers.jsonPath("$.queue").exists());
 	}
 
 }
